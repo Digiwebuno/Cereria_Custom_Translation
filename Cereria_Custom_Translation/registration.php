ff624853689efc6c6e5535d8beca3ba26d1f4cad
ErikDW1 <erik.capoccetta@digiwebuno.it>
2024-04-03 12:34:37 +0200

    First Commit

---
+++ Cereria_Custom_Translation/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Digiwebuno_Cereria_Custom_Translation',
+    __DIR__
+);
